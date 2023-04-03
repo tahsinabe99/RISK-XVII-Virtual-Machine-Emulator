1be3ccbfcd90b9ebb7565f3e4d3774b71bfea966
Tahsinul Abedin <tahsinabe99@gmail.com>
2023-04-03 11:15:19 -0400

    error fixed

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -117,7 +117,7 @@ void virtual_routines(int memory_address, int stored_value){
 //return 0 if the memory is not accessed
 //returns 1, if memory is accessed and carries on virtual routine
 int check_virtual_memory_access(int memory_address, int value){
-    if(memory_address==0x800 | memory_address==0x804 | memory_address==0x808 |memory_address==0x80C | memory_address==0x812 | memory_address==0x816 | memory_address==0x820 | memory_address==0x824 | memory_address==0x828  ){
+    if((memory_address==0x800 )| (memory_address==0x804) | (memory_address==0x808) | (memory_address==0x80C) | (memory_address==0x812) | (memory_address==0x816) |  (memory_address==0x820) | (memory_address==0x824) | (memory_address==0x828) ){
         //printf("MEMEORY ACCCESSED!!: %08x\n", memory_address);
         virtual_routines(memory_address, value);
         return 1;
