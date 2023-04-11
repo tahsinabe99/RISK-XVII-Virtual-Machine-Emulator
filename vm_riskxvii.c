2d7fdff1bace4c7d46107e56e7624f6ccd4f5d1c
Tahsinul Abedin <tahsinabe99@gmail.com>
2023-04-11 13:47:15 -0400

    trying to reduce binary size

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -6,7 +6,7 @@ int memory[2303];
 short int pc=0;
 
 
-
+//we use this method to extract  binary of specific length 
 int break_binary2(int instruction, int start, int end){
     int bitmask=((1 << (end-start+1))-1) << start;
     int broken_binary= (instruction & bitmask) >> start;
@@ -15,6 +15,7 @@ int break_binary2(int instruction, int start, int end){
 
 //modify the giventh bit
 int modifyBit(int binary_number, int position, int bit){
+    //
     int bit_mask =1 << position;
     int changed_num=((binary_number & ~bit_mask) | (bit << position));
     return changed_num;
@@ -62,8 +63,8 @@ void heap_banks(){
 //virtual routines
 void virtual_routines(int memory_address, int stored_value){
     if(memory_address==0x800){
-        //printf("console write character\n");
         memory[0x800]=stored_value;
+        //we use casting to ensure the value that will be printed is of char type
         printf("%c",(char) memory[0x800]);
     }
     else if(memory_address==0x804){
@@ -122,8 +123,10 @@ int check_virtual_memory_access(int memory_address, int value){
 //instruction is stored as int in int array
 //to access which byte of instruction memory, we use this 
 int instruction_memory_access(int x){
+    //we calculate which instruction needs to be extracted
     int instruction_no=x/4; //not pc value
     int instruction_pc_no=instruction_no*4;
+    //diff bits shows the starting position of bits to be extracted
     int diff= x-instruction_pc_no;
     return (diff*8);
 }
@@ -144,6 +147,8 @@ void sub(int rd, int rs1, int rs2){
     pc=pc+4;
 }
 
+//xor gives one if bits of two operant is opposite
+//else it gives 0; (1^1=0)(1^0=1)
 void xor(int rd, int rs1, int rs2){
     if(rd!=0b00){
         registers[rd]= registers[rs1]^registers[rs2];
@@ -151,6 +156,8 @@ void xor(int rd, int rs1, int rs2){
     pc=pc+4;
 }
 
+//or operator gives 1 if one of the bits is 1
+//else it gives 1
 void or(int rd, int rs1, int rs2){
     if(rd!=0b00){
         registers[rd]= registers[rs1] | registers[rs2];
@@ -158,6 +165,7 @@ void or(int rd, int rs1, int rs2){
     pc=pc+4;
 }
 
+//gives 1 if both bits is 1, else it gives 0
 void and(int rd, int rs1, int rs2){
     if(rd!=0b00){
         registers[rd]= registers[rs1] & registers[rs2];
@@ -165,7 +173,7 @@ void and(int rd, int rs1, int rs2){
     pc=pc+4;
 }
 
-
+//shifts the bits of rs1 to left by value of rs2 and stores in rd
 void sll(int rd, int rs1, int rs2){
     if(rd!=0){
         registers[rd]=registers[rs1] << registers[rs2];
@@ -173,6 +181,7 @@ void sll(int rd, int rs1, int rs2){
     pc=pc+4;
 }
 
+//shifts the bits of rs1 to right by value of rs2 and stores in rd
 void srl(int rd, int rs1, int rs2){
     if(rd!=0){
         registers[rd]=registers[rs1] >> registers[rs2];
@@ -180,16 +189,22 @@ void srl(int rd, int rs1, int rs2){
     pc=pc+4;
 }
 
+//shifts the bits of rs1 to right by value of rs2
+//bits that are dropped of due to right shift is added in the left most bits
 void sra(int rd, int rs1, int rs2){
     if(rd!=0){
+        //right most bits that will fall off is stored and shiftited to most significant bits
         int right_most_bits= break_binary2(registers[rs1],0,(registers[rs2]-1));
         right_most_bits= right_most_bits << (32-registers[rs2]);
         registers[rd]=registers[rs1] >> registers[rs2];
+        //bits that fell off is added to the msb
+        //or operator is used to attach the fallen off bits as the msb of the shifted number are zero
         registers[rd] =registers[rd] | right_most_bits;
     }
     pc=pc+4;
 }
 
+//if rs1 < than rs 2, rd is 1, else 0;
 void slt(int rd, int rs1, int rs2){
     if(registers[rs1] < registers[rs2]){
         registers[rd]=1;
@@ -251,6 +266,8 @@ void addi(int rd, int rs1, int imm){
     pc=pc+4;
 }
 
+//same as xor, but done with imm
+//returns 1 if bits of 2 operand is opposite
 void xori(int rd, int rs1, int imm){
     if(rd!=0b00000){
         registers[rd]=registers[rs1]^imm;
@@ -258,6 +275,7 @@ void xori(int rd, int rs1, int imm){
     pc=pc+4;
 }
 
+//same as or just with imm
 void ori(int rd, int rs1, int imm){
     if(rd!=0b00000){
         registers[rd]=registers[rs1] | imm;
@@ -272,6 +290,7 @@ void andi(int rd, int rs1, int imm){
     pc=pc+4;
 }
 
+//sets rd to pc+4; pc= rs1+imm
 void jalr(int rd, int rs1, int imm){
     if(rd!=0b00000){
         registers[rd]=pc+4;
@@ -279,6 +298,7 @@ void jalr(int rd, int rs1, int imm){
     pc=registers[rs1]+imm;
 }  
 
+//sets rd to 1 if rs1<imm; else rd is 0
 void slti(int rd, int rs1, int imm){
     if(rd!=0){
         if(registers[rs1] < imm){
@@ -292,12 +312,16 @@ void slti(int rd, int rs1, int imm){
     pc=pc+4;
 }
 
-
+//load a 32 bit value from memory into rd
+//
 void lw(int rd, int rs1, int imm){
     int memory_address=registers[rs1]+imm;
     int value= memory[memory_address];
 
     if(rd!=0){
+        //if memory address is in instruction memory we set the address/4
+        //because instruction is set in an integer array
+        //memory address acts as index to memory array.
         if( (memory_address>=0x00) && (memory_address<0x3ff) ){
             value=memory[memory_address/4];
         }
@@ -317,10 +341,12 @@ void lw(int rd, int rs1, int imm){
 void lbu(int rd, int rs1, int imm){
     int memory_address=registers[rs1]+imm;
     int value= memory[memory_address];
-    //printf("before memory address lbu %08x\n", memory_address);
-    //check_virtual_memory_access(memory_address, value);
+    //loads a 8bit value from memory into register
     if(rd!=0){
+        //we check if it is instruction memory
+        //if insturction memory we need to divide memory address by 4 so it acts as index to which instruction
         if( (memory_address>=0x00) && (memory_address<0x7ff) ){
+            //which bits need to be extracted is calculates in bit start
             int bit_start=instruction_memory_access(memory_address);
             value=memory[memory_address/4];
             value=break_binary2(value, bit_start, (bit_start+7));
@@ -392,10 +418,8 @@ void type_i( int instruction){
 //store a 8 bit value to memory from a register
 void sb(int rs1, int rs2, int imm){
     int value= registers[rs2];
-
-    //to be continued from here
     int memory_address= registers[rs1]+imm;
-    
+    //check if it tries to write instruction memory
     if( !( (memory_address>=0x00) && (memory_address<0x3ff) )){
         //value should be before virtual memory access
         //check_virtual_memory_access(memory_address, value);
@@ -407,16 +431,12 @@ void sb(int rs1, int rs2, int imm){
     
     pc=pc+4;
 }
-
+//store a 16 bit value to memory from a register
 void sh(int rs1, int rs2, int imm){
     int value= registers[rs2];
-    //value=break_binary(value, 0, 8);
-
-    //to be continued from here
     int memory_address= registers[rs1]+imm;
     
     if( !( (memory_address>=0x00) && (memory_address<0x3ff) )){
-        //check_virtual_memory_access(memory_address, value);
         value=break_binary2(registers[rs2], 0,15);
         check_virtual_memory_access(memory_address, value);
         memory[registers[rs1]+imm]=value;
@@ -426,6 +446,7 @@ void sh(int rs1, int rs2, int imm){
     pc=pc+4;
 }
 
+//store a 32 bit value to memory from register 
 void sw(int rs1,int rs2, int imm){
     int memory_address=registers[rs1]+imm;
     if(memory_address>=0xb700 && memory_address<=0xd700){
@@ -447,9 +468,8 @@ void sw(int rs1,int rs2, int imm){
 
 int imm_manipulate_S(int imm1, int imm2){
     int immediate_num= imm2 << 5; //| imm1; 
+    //imm2[11:5] imm1[4:0]
     immediate_num=immediate_num | imm1;
-    //printf("this is stype imm:%08x\n", immediate_num);
-    //displayBits(immediate_num);
     return immediate_num;
 }
 
@@ -460,19 +480,18 @@ void type_s(int instruction){
     int rs1=break_binary2(instruction, 15,19);
     int rs2=break_binary2(instruction,20,24);
     int imm2=break_binary2(instruction, 25,31);
-    //displayBits(imm1);
-    //displayBits(imm2);
     int immediate_num=imm_manipulate_S(imm1, imm2);
 
     if(instruction<0){
-        //printf("negative\n");
         immediate_num=modifyBit(immediate_num, 11, 1);
         int bitmask=0b0;
+        //~bit complementl; turns 0 to 1 and 1 to 0
         bitmask=~bitmask;
+        //making space for 11 bit immediate number
         bitmask=bitmask<<11;
+        //msb will be 1 and after that will be imm value
         immediate_num=bitmask | immediate_num;
     }
-    //printf("This is imm type S:n %d\n", immediate_num);
 
     if(func3==0b000){
         sb(rs1, rs2, immediate_num);
@@ -493,12 +512,10 @@ void type_s(int instruction){
 void beq(int rs1, int rs2,int imm){
     if(registers[rs1]==registers[rs2]){
         pc=pc+imm;
-        //printf("triggered\n");
     }
     else{
         pc=pc+4;
     }
-    //printf("beq is done andn pc:%08x\n", pc);
 }
 
 
@@ -508,7 +525,6 @@ void bne(int rs1, int rs2,int imm){
     }else{
         pc=pc+4;
     }
-    //printf("bne is done andn pc:%08x\n", pc);
 }
 
 void blt(int rs1, int rs2,int imm){
@@ -517,7 +533,6 @@ void blt(int rs1, int rs2,int imm){
     }else{
         pc=pc+4;
     }
-    //printf("blt is done andn pc:%08x\n", pc);
 }
 
 void bge(int rs1, int rs2,int imm){
@@ -526,9 +541,9 @@ void bge(int rs1, int rs2,int imm){
     }else{
         pc=pc+4;
     }
-    //printf("bne is done andn pc:%08x\n", pc);
 }
 
+//branch if less than// treat numbers unsigned
 void bltu(int rs1, int rs2,int imm){
     //casting the values to treat them as unsigned.
     unsigned r1= (unsigned) (registers[rs1]);
@@ -539,11 +554,10 @@ void bltu(int rs1, int rs2,int imm){
     }else{
         pc=pc+4;
     }
-    //printf("blt is done andn pc:%08x\n", pc);
 }
 
 void bgeu(int rs1, int rs2, int imm){
-
+    //treat numbers as unsigned
     unsigned r1= (unsigned) (registers[rs1]);
     unsigned r2= (unsigned) (registers[rs2]);
 
@@ -553,24 +567,19 @@ void bgeu(int rs1, int rs2, int imm){
     }else{
         pc=pc+4;
     }
-    //printf("bgeu is done andn pc:%08x\n", pc);
 }
 
+//imm of sb is imm1[4:1 |11] imm2[12 | 10:5]
 int imm_manipulate_SB(int imm1, int imm2){
     int part11=break_binary2(imm1,0,0);
-    //displayBits(part11);
     int part1_4=break_binary2(imm1, 1,4);
-    //displayBits(part1_4);
     int part10_5=break_binary2(imm2,0,5);
-    //displayBits(part10_5);
     int part12=break_binary2(imm2,6, 6);
-    //displayBits(part12);
     int immediate_num= part12 << 1 | part11;                        
     immediate_num= immediate_num << 6 | part10_5;
     immediate_num=immediate_num << 4 | part1_4;
     //we assume imm[0] is 0 so we bit shift it to left to give least significat bit 0;
     immediate_num= immediate_num << 1;
-    //displayBits(immediate_num);
     return immediate_num;
 }
 
@@ -589,9 +598,6 @@ void type_sb(int instruction){
         bitmask=bitmask<<11;
         immediate_num=bitmask | immediate_num;
     }
-    //printf("This is imm of SB: %d\n", immediate_num);
-    
-    //displayBits(immediate_num);
 
     if(func3==0b000){
         beq(rs1,rs2,immediate_num);
@@ -611,9 +617,6 @@ void type_sb(int instruction){
     else if(func3==0b110){
         bltu(rs1, rs2, immediate_num);
     }
-    // else if(func3==0b111) {
-    //     bgeu(rs1, rs2, immediate_num);
-    // }
     else{
         unknown_instruction(instruction);
     }
@@ -621,7 +624,8 @@ void type_sb(int instruction){
 }
 
 //u type starts
-
+//just loads the upper immediate of imm as rd
+//since we dont add 0s in 11;0 of imm we left shift imm by 12 and rd=imm
 void lui(int rd, int imm){
     if(rd!=0b00000){
         int value=imm;
@@ -707,8 +711,6 @@ void type_uj(int instruction){
 void carry_instruction(int instruction){
     int opcode= break_binary2(instruction, 0,6);
     int type=opcode_type_check(opcode);
-    //printf("This is instruction: %08x,of type: %d\n", instruction, type);
-
     if(type==1){
         type_r(instruction);
     }
